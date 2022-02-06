2741ffb4f47584de7c03ececc05dcd9dfa577fd6
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-06 20:27:32 +0530

    JS problems

---
+++ practice.js
@@ -0,0 +1,13 @@
+function sort(arr) {
+    for (var i = 1; i < arr.length; i++)
+        for (var j = 0; j < i; j++)
+            if (arr[i] < arr[j]) {
+                var x = arr[i];
+                arr[i] = arr[j];
+                arr[j] = x;
+            }
+    return arr
+}
+
+let arr = [9,4,6,1,2,3,5,8,7]
+console.log(sort(arr))
