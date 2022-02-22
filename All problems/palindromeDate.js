38039a79355bf36c0ebd3b4262819680c21513ca
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-22 23:46:09 +0530

    JS Problems

--- All problems/palindromeDate.js
+++ All problems/palindromeDate.js
@@ -3,36 +3,36 @@
 
 //20220222 -> 22202202
 
-// const date = new Date();
+const date = new Date();
 
-// function isDatePalindrome() {
-//   let year = date.getFullYear();
-//   let month = date.getMonth() + 1;
-//   let dt = date.getDate();
+function isDatePalindrome() {
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dt = date.getDate();
 
-//   if (dt < 10) {
-//     dt = "0" + dt;
-//   }
-//   if (month < 10) {
-//     month = "0" + month;
-//   }
-//   let todayDate = dt + month + year;
-//   const rev = todayDate.split("").reverse().join("");
-
-//   if (todayDate == rev) {
-//     return `Today's date is palindrome.`;
-//   }
-//   return `Today's date is not palindrome.`;
-// }
-// console.log(isDatePalindrome(date));
-
-function date(str) {
-  let xx = str.split("").reverse().join("");
-  if (xx == str) {
-    return true;
+  if (dt < 10) {
+    dt = "0" + dt;
   }
-  return false;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  let todayDate = dt + month + year;
+  const rev = todayDate.split("").reverse().join("");
+
+  if (todayDate == rev) {
+    return `Today's date is palindrome.`;
+  }
+  return `Today's date is not palindrome.`;
 }
-console.log(date("22022022"));
+console.log(isDatePalindrome(date));
+
+// function date(str) {
+//   let xx = str.split("").reverse().join("");
+//   if (xx == str) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(date("22022022"));
 
 // 2222022  - >  2202222
