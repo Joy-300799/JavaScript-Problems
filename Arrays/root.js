53810a905df96fd5684d8244fd5386a94794a18e
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-27 21:52:45 +0530

    JS Problems

--- Arrays/root.js
+++ Arrays/root.js
@@ -1,1 +1,19 @@
-const arr = [3,4,2,7,8,5,1,9];
+const arr = [25,81,225,8];
+// calculate the sum of sq. root of the max value and cb. root of the min value.
+
+function sumOfRoots(arr) {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+
+//   console.log(max,"maximum value")
+//   console.log(min,"minimum value")
+  
+  let sqRt = Math.sqrt(max);
+  let cbRt = Math.cbrt(min);
+
+//   console.log(sqRt,"sqrt")
+//   console.log(cbRt,"cbrt")
+
+  return sqRt + cbRt;
+}
+console.log(sumOfRoots(arr));
