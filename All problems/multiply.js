d20210dd04fca195c90088c230719b934bfb60a7
Joy bhattacharya <joy.arya7381@gmail.com>
2022-03-07 21:02:41 +0530

    JS Problems

---
+++ All problems/multiply.js
@@ -0,0 +1,14 @@
+//Multiply function without using * operator.
+
+let num1 = 5;
+let num2 = 3;
+
+function Multiply(num1, num2) {
+  if (num1 == 0 || num2 == 0) {
+    return 0;
+  } else {
+    console.log(num1);
+    return num1 + Multiply(num1, num2 - 1);
+  }
+}
+console.log(Multiply(num1,num2))
